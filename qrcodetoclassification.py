4d082c52f17cf694a928c82496a658206077b623
RicardaBeckmann <ricarda.beckmann@astro.ox.ac.uk>
2016-06-22 16:40:35 +0100

    Decide if users are in the right place based on the clipboard data
    The function regularly reads the clipboard data and then decides if the user is
    in the correct place for the current station. If so, the celebration can be
    triggered. If not, an alternative scenario can be set up.
    
    Currently classification_data.csv is a dummy file. It needs to be filled
    with the real data in the format Id,bulge,bar.

--- qrcodetoclassification.py
+++ qrcodetoclassification.py
@@ -1,14 +1,60 @@
 import time
 import pyperclip
+import csv
 
-clipboard_old = ""
+#local information
 
-while True:
 
-    clipboard = pyperclip.paste()
+def check_status(bar=1,bulge=0):
+        
+    clipboard_old=""
+    while True:
+        clipboard = pyperclip.paste()
+        
+        if (clipboard != clipboard_old):
+            print "New ID!",clipboard
+            clipboard_old = clipboard
 
-    if (clipboard != clipboard_old):
-        print "New ID!"
-        clipboard_old = clipboard
+            #Load data object for that classification
+            # Have lookup table of the form id, bar, bulge  where bar&bulge are out of 1,0
 
-    time.sleep(0.5)
+            classification=read_object_classification(clipboard_old)  #in the form [id,bar,bulge]
+            #classification=['1ds4',1,0]    #example of a barred galaxy withotu a bulge
+            print "Galaxy data",classification,"Location data",bar,bulge
+            status=bar==classification[1] and bulge==classification[2]
+
+            if status:
+                print "Success :) Do the things!"
+
+            else:
+                print "Fail :( No bubbles for you"
+
+            print '-------------'
+
+        time.sleep(0.5)
+
+
+def read_object_classification(clipboard_old):
+    filename="classification_data.csv"
+    with open(filename) as f:
+        reader=csv.reader(f,delimiter=',')
+        next(reader)
+        for row in reader:
+            if row[0]==str(clipboard_old):
+                row=[int(item) for item in row]
+                return row
+        print "Id not found. Return dummy data"
+        return ['0000000',2,2]
+
+
+
+def write_example_file():
+    filename="classification_data.csv"
+    IDS=['1243233','2345473','2233432','9987679','3345363','3934322']
+    bulge=[0,0,0,1,1,1]
+    bar=[1,0,0,1,0,1]
+    with open(filename,'w') as f:
+        writer=csv.writer(f)
+        writer.writerow(['Id','bulge','bar'])
+        for i in range(len(IDS)):
+            writer.writerow([IDS[i],bulge[i],bar[i]])
